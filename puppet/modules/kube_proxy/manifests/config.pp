00c22a1a4a83448c5454b0c0ce31b3053b50ccb0
Jose Fernandez (magec) <joseferper@gmail.com>
2021-03-09 01:20:26 +0000

    Fix config command

--- puppet/modules/kube_proxy/manifests/config.pp
+++ puppet/modules/kube_proxy/manifests/config.pp
@@ -14,7 +14,7 @@ class kube_proxy::config(
     hostnames => [],
   } -> kubectl::config { "${kube_common::conf_dir}/kube-proxy.kubeconfig":
     commands => [
-      "set-cluster ${kube_common::cluster_name} --certificate-authority=${kube_common::conf_dir}/ca.pem --embed-certs=true --server=https://127.0.0.1:6443",
+      "set-cluster ${kube_common::cluster_name} --certificate-authority=${kube_common::conf_dir}/ca.pem --embed-certs=true --server=https://${kube_common::public_address}:6443",
       "set-credentials system:kube-proxy --client-certificate=${kube_common::conf_dir}/kube-proxy.pem --client-key=${kube_common::conf_dir}/kube-proxy-key.pem --embed-certs=true",
       "set-context default --cluster=${kube_common::cluster_name} --user=system:kube-proxy",
       "use-context default"
